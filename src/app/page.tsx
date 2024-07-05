e442c845f6da9040ea39ac494721a0b6800c64dd
remilebeau <remilebeau90@gmail.com>
2024-07-05 19:35:16 -0400

    style homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,8 @@ import DataForm from "@/components/DataForm";
 export default function HomePage() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
-      <h1 className="text-3xl font-bold">Cash Flow Simulator</h1>
-      <p>
-        Select a distribution and input the parameters. Using Monte Carlo
-        simulation, these values will be used to forecast annual cash flow.
-        These models can also be used to forecast demand.
-      </p>
+      <h1 className="text-3xl font-bold">Monte Carlo Simulation</h1>
+      <p>Select a model and input the parameters.</p>
       <DataForm />
       <ThemeSwitch />
     </main>
