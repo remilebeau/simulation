303f9b414b79c547cc537dff5b05805e5221b9be
remilebeau <remilebeau90@gmail.com>
2024-07-15 19:17:52 -0400

    homepage styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function HomePage() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
       <h1 className="text-3xl font-bold">Monte Carlo Simulation</h1>
-      <p>Select a model or distribution and input the parameters.</p>
+      <p>Select a model or distribution and input the parameters</p>
       <p>
         To learn more about the assumptions of the models, or to download the
         values for your own analysis, please visit the{" "}
@@ -19,8 +19,8 @@ export default function HomePage() {
         </a>
       </p>
       <p>
-        When inputting values, do not flip the sign (e.g. setting revenues
-        positive and costs negative)
+        When entering values for the models, do not flip the sign (e.g. making
+        revenues positive and costs negative)
       </p>
       <SelectOption />
       <ThemeSwitch />
