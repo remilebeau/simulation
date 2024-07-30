4fd9fe5785dea87fddf7b776c80f8fa7efbb6791
remilebeau <remilebeau90@gmail.com>
2024-07-30 11:41:00 -0400

    add lib function to call /api/simulations/cash_flow

---
+++ src/lib/simulateCashFlow.ts
@@ -0,0 +1,30 @@
+import { redirect } from "next/navigation";
+
+type ResponseType = {
+  annualCashFlows: number[];
+};
+
+export async function simulateCashFlow(
+  periodsPerYear: number,
+  min: string,
+  mean: string,
+  max: string,
+  sd: string,
+): Promise<ResponseType> {
+  const DATA_URL =
+    process.env.NODE_ENV === "production"
+      ? `https://simulation-api-rsaw.onrender.com/api/simulations/cash_flow?periodsPerYear=${periodsPerYear}&min=${min}&mean=${mean}&max=${max}&sd=${sd}`
+      : `http://localhost:8000/api/simulations/cash_flow?periodsPerYear=${periodsPerYear}&min=${min}&mean=${mean}&max=${max}&sd=${sd}`;
+  const res = await fetch(DATA_URL, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  if (!res.ok) {
+    redirect("/");
+  }
+  return res.json();
+}
+
+export default simulateCashFlow;
