7fde73c3acbe857d1839390544c0220e22334c04
remilebeau <remilebeau90@gmail.com>
2024-08-02 16:39:24 -0400

    set BackButton.tsx to 100% width

--- src/components/BackButton.tsx
+++ src/components/BackButton.tsx
@@ -4,7 +4,10 @@ import { useRouter } from "next/navigation";
 export default function BackButton() {
   const router = useRouter();
   return (
-    <Button className="text-2xl font-bold" onClick={() => router.push("/")}>
+    <Button
+      className="w-full text-2xl font-bold"
+      onClick={() => router.push("/")}
+    >
       Go Back
     </Button>
   );
