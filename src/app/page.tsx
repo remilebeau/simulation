1cfa1dc05db4ec934af82a3181836c36ac2aa52a
remilebeau <remilebeau90@gmail.com>
2024-08-02 16:41:20 -0400

    remove typography from globals.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,11 @@ import SelectOption from "@/components/SelectOption";
 export default function HomePage() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col gap-8 p-8">
-      <h1>Monte Carlo Simulation</h1>
-      <p>Select a model or distribution and input the parameters</p>
-      <p>
+      <h1 className="text-center text-5xl">Monte Carlo Simulation</h1>
+      <p className="text-xl">
+        Select a model or distribution and input the parameters
+      </p>
+      <p className="text-xl">
         To learn more about the assumptions of the models, or to download the
         values for your own analysis, please visit the{" "}
         <a
@@ -18,11 +20,11 @@ export default function HomePage() {
           API docs
         </a>
       </p>
-      <p>
+      <p className="text-xl">
         When entering inputs for the models, do not flip the sign (e.g. making
         revenues positive and costs negative)
       </p>
-      <p>
+      <p className="text-xl">
         When using the models, demand must follow one of these distributions:
         triangular, truncated normal, uniform, or normal
       </p>
