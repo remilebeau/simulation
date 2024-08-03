870fec9f54df2542e4da518b6564343acfe0a156
remilebeau <remilebeau90@gmail.com>
2024-08-03 19:52:57 -0400

    remove bg color from labels

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -7,7 +7,7 @@ import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
 const labelVariants = cva(
-  "bg-primary text-primary-foreground w-1/3 text-center p-2 rounded-xl text-xl font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
+  "rounded-xl text-xl font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70",
 );
 
 const Label = React.forwardRef<
