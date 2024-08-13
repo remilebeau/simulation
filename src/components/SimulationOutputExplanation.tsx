1be9e73a4606a7bd448758ce5e9e4bed6f7830d9
remilebeau <remilebeau90@gmail.com>
2024-08-12 20:56:28 -0400

    add content to simulation output explanation

--- src/components/SimulationOutputExplanation.tsx
+++ src/components/SimulationOutputExplanation.tsx
@@ -15,19 +15,20 @@ export default function SimulationOutputExplanation() {
       </p>
       <p>
         <b>95% Confidence Interval for Mean:</b> The 95% confidence interval for
-        the expected profit. The expected profit will lie within this interval
-        95% of the time. Note: This is the confidence interval for the MEAN or
-        EXPECTED profit. This does NOT mean that there's a 95% chance the profit
-        from a single simulation will fall within this interval.
+        the expected profit. We are 95% confident that the expected profit will
+        fall within this interval. Note: This is the confidence interval for the
+        MEAN or EXPECTED profit. This does NOT mean that there's a 95% chance
+        the profit from a single simulation will fall within this interval.
       </p>
       <p>
-        <b>Probability of Losing Money:</b> The probability of losing money.
-        This is calculated by counting the number of simulations that resulted
-        in a negative profit and dividing that number by the total number of
-        simulations, which in our case is 1000. For example, if 200 profit
-        values were below 0, then the probability of losing money is
-        approximately 20%. From this point estimate, we can also calculate a 95%
-        confidence interval.
+        <b>Probability of Losing Money:</b> The 95% confidence interval for the
+        probability of losing money. This is calculated by counting the number
+        of simulations that resulted in a negative profit and dividing that
+        number by the total number of simulations, which in our case is 1000.
+        For example, if 200 profit values were below 0, then the probability of
+        losing money is approximately 20%. From this point estimate, we can also
+        calculate a 95% confidence interval. We are 95% confident that the
+        probability of losing money will fall within this interval.
       </p>
       <p>
         <b>Value at Risk at 5% Level:</b> The value at risk at the 5% level. If
