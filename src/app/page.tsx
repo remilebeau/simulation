d7808c19ffcb0b6063433460f78dd65f1132f23b
remilebeau <remilebeau90@gmail.com>
2024-08-26 14:48:18 -0400

    center homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import SelectOption from "@/components/SelectOption";
 
 export default function HomePage() {
   return (
-    <main className="mx-auto flex max-w-4xl flex-col gap-4 p-4">
-      <h1 className="rounded-xl bg-primary p-4 text-center text-xl font-bold text-primary-foreground">
+    <main className="mx-auto flex max-w-4xl flex-col items-center gap-4 p-4">
+      <h1 className="w-full rounded-xl bg-primary p-4 text-center text-xl font-bold text-primary-foreground">
         Monte Carlo Simulation
       </h1>
       <p>
