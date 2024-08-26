6c1f625f78251f3b7d980eae176a853428ca9aa0
remilebeau <remilebeau90@gmail.com>
2024-08-25 21:01:16 -0400

    update FinanceInstructions

--- src/components/FinanceInstructions.tsx
+++ src/components/FinanceInstructions.tsx
@@ -1,24 +1,22 @@
 export default function FinanceInstructions() {
   return (
     <section className="flex flex-col gap-4 p-4">
-      <h2 className="text-center text-2xl">
-        Finance Model Parameters Explained
-      </h2>
+      <h2 className="font-bold">Finance Model Parameters</h2>
       <p>
         <b>Fixed Costs:</b> The total fixed costs over the 5 year planning
-        horizon
+        horizon.
       </p>
       <p>
         <b>Year One Margin:</b> The gross profit in year one.
       </p>
       <p>
-        <b>Year One Sales Min:</b> The minimum sales in year one
+        <b>Year One Sales Min:</b> The minimum sales in year one.
       </p>
       <p>
-        <b>Year One Sales Mode:</b> The expected sales in year one
+        <b>Year One Sales Mode:</b> The expected sales in year one.
       </p>
       <p>
-        <b>Year One Sales Max:</b> The maximum sales in year one
+        <b>Year One Sales Max:</b> The maximum sales in year one.
       </p>
       <p>
         <b>Annual Margin Decrease:</b> The percentage by which gross profit will
