80a35af5d39b6dd45c66897666c5cc61a6b3b397
remilebeau <remilebeau90@gmail.com>
2024-08-25 20:57:54 -0400

    update ProductionInstructions.tsx

--- src/components/ProductionInstructions.tsx
+++ src/components/ProductionInstructions.tsx
@@ -1,31 +1,29 @@
 export default function ProductionInstructions() {
   return (
     <section className="flex flex-col gap-4 p-4">
-      <h2 className="text-center text-2xl">
-        Production Model Parameters Explained
-      </h2>
+      <h2 className="font-bold">Production Model Parameters</h2>
       <p>
-        <b>Unit cost:</b> The variable costs per unit
+        <b>Unit Cost:</b> The variable costs per unit.
       </p>
       <p>
-        <b>Unit price:</b> The sell price per unit
+        <b>Unit Price:</b> The sell price per unit.
       </p>
       <p>
-        <b>Salvage price:</b> The salvage value for each unit produced above
-        demand
+        <b>Salvage Price:</b> The salvage value for each unit produced above
+        demand.
       </p>
       <p>
-        <b>Demand Distribution</b> The expected distribution of demand. Demand
+        <b>Demand Distribution</b>: The expected distribution of demand. Demand
         is typically forecasted using historical data, industry knowledge,
         conversations with the sales and marketing team, and other means. This
         model accepts triangular, normal, uniform, or truncated normal demand
         distributions.
       </p>
       <p>
-        <b>Fixed costs:</b> The total fixed costs of the production
+        <b>Fixed costs:</b> The total fixed costs of the production.
       </p>
       <p>
-        <b>Production Quantity:</b> The number of units to be produced
+        <b>Production Quantity:</b> The number of units to be produced.
       </p>
     </section>
   );
