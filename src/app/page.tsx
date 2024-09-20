b99fe849e89fd8c867669284ce4f5d534d81f7a0
remilebeau <remilebeau90@gmail.com>
2024-09-20 13:41:40 -0500

    only use production planning simulation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
-import SelectOption from "@/components/SelectOption";
 import HomePageInstructions from "@/components/HomePageInstructions";
+import dynamic from "next/dynamic";
 
 export default function HomePage() {
+  // import ProductionForm
+  const ProductionForm = dynamic(() => import("@/components/ProductionForm"), {
+    ssr: false,
+  });
   return (
     <main className="mx-auto flex max-w-4xl flex-col items-center gap-4 p-4">
       <h1 className="w-full rounded-xl bg-teal-700 p-4 text-center text-xl font-bold">
         Monte Carlo Simulation
       </h1>
       <HomePageInstructions />
-      <SelectOption />
+      <ProductionForm />
     </main>
   );
 }
