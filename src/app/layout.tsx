95949cba011e2aec0078307b63a2c5e740d70887
remilebeau <remilebeau90@gmail.com>
2024-09-20 13:05:09 -0500

    change bg, header, and font color. remove theme switch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import { ThemeProvider } from "@/components/theme-provider";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -16,16 +15,9 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning>
-      <body className={`${inter.className} bg-background`}>
-        <ThemeProvider
-          attribute="class"
-          defaultTheme="system"
-          enableSystem
-          disableTransitionOnChange
-        >
-          {children}
-        </ThemeProvider>
+    <html>
+      <body className={`bg-black text-white ${inter.className}`}>
+        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,14 @@
-import { ModeToggle as ThemeSwitch } from "@/components/ThemeSwitch";
 import SelectOption from "@/components/SelectOption";
 import HomePageInstructions from "@/components/HomePageInstructions";
 
 export default function HomePage() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col items-center gap-4 p-4">
-      <h1 className="w-full rounded-xl bg-primary p-4 text-center text-xl font-bold text-primary-foreground">
+      <h1 className="w-full rounded-xl bg-teal-700 p-4 text-center text-xl font-bold">
         Monte Carlo Simulation
       </h1>
       <HomePageInstructions />
       <SelectOption />
-      <ThemeSwitch />
     </main>
   );
 }
