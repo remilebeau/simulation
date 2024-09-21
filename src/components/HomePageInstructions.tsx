edeea723eb3b503285e8e85b73847be0822cc2e3
remilebeau <remilebeau90@gmail.com>
2024-09-21 12:17:08 -0500

    styling

--- src/components/HomePageInstructions.tsx
+++ src/components/HomePageInstructions.tsx
@@ -4,8 +4,8 @@ export default function HomePageInstructions() {
       <section className="flex flex-col gap-4 p-4">
         <p>
           Input the parameters of your production scenario. The scenario will be
-          simulated 1000 times, and the results will be summarized in a
-          histogram.
+          simulated 1000 times, and the resulting 1000 profits will be
+          summarized in a histogram.
         </p>
         <p>
           From this histogram, we can answer the following questions about our
@@ -13,39 +13,19 @@ export default function HomePageInstructions() {
         </p>
         <ul className="flex flex-col gap-2 sm:grid sm:grid-cols-2">
           <li>What is the expected profit?</li>
-          <li>What is the chance of a profit above a required value?</li>
+          <li>What is the chance of a profit above a required amount?</li>
           <li>What was the lowest profit observed?</li>
           <li>What was the highest profit observed?</li>
-          <li>What is the risk of losing money?</li>
+          <li>What is the chance of a negative profit?</li>
           <li>What is the value at risk?</li>
         </ul>
       </section>
       <section className="flex flex-col gap-4 p-4">
         <p>
-          A key factor in Monte Carlo simulation is the choice of probability
-          distribution for any random variables, such as demand. Although demand
-          is random, thanks to our historical data, industry knowledge, and
-          conversations with our sales and marketing team, we typically have{" "}
-          <i>some</i> idea of the randomness.
+          Although demand is random, thanks to our historical data, industry
+          knowledge, conversations with our sales and marketing team, and other
+          means, we typically have <i>some</i> idea of the randomness.
         </p>
-        <p>This app accepts the following distributions:</p>
-        <ul className="flex flex-col gap-4 p-4 sm:grid sm:grid-cols-2">
-          <li>
-            <img src="/normal.png" alt="Normal Distribution" />
-          </li>
-          <li>
-            <img src="/triangular.png" alt="Triangular Distribution" />
-          </li>
-          <li>
-            <img
-              src="/truncatedNormal.png"
-              alt="Truncated Normal Distribution"
-            />
-          </li>
-          <li>
-            <img src="/uniform.png" alt="Uniform Distribution" />
-          </li>
-        </ul>
       </section>
       <p>
         To download the raw output data for your own analysis, please visit the{" "}
