873d4fc76a12949322aca32b10e74f7c13024e4c
remilebeau <remilebeau90@gmail.com>
2024-09-25 22:13:40 -0400

    styling

--- src/components/ProductionInstructions.tsx
+++ src/components/ProductionInstructions.tsx
@@ -2,25 +2,27 @@ export default function ProductionInstructions() {
   return (
     <section className="flex flex-col gap-4">
       <h2 className="font-bold">Production Planning Model Parameters</h2>
-      <p>
-        <b>Unit Cost:</b> The variable costs per unit.
-      </p>
-      <p>
-        <b>Unit Price:</b> The sell price per unit.
-      </p>
-      <p>
-        <b>Salvage Price:</b> The salvage value for each unit produced above
-        demand.
-      </p>
-      <p>
-        <b>Demand Distribution</b>: The expected distribution of demand.
-      </p>
-      <p>
-        <b>Fixed Costs:</b> The total fixed costs of the production.
-      </p>
-      <p>
-        <b>Production Quantity:</b> The number of units to be produced.
-      </p>
+      <ul className="flex flex-col gap-2 sm:grid sm:grid-cols-2">
+        <li>
+          <b>Unit Cost:</b> The variable costs per unit.
+        </li>
+        <li>
+          <b>Unit Price:</b> The sell price per unit.
+        </li>
+        <li>
+          <b>Salvage Price:</b> The salvage value for each unit produced above
+          demand.
+        </li>
+        <li>
+          <b>Demand Distribution:</b> The expected distribution of demand.
+        </li>
+        <li>
+          <b>Fixed Costs:</b> The total fixed costs of the production.
+        </li>
+        <li>
+          <b>Production Quantity:</b> The number of units to be produced.
+        </li>
+      </ul>
     </section>
   );
 }
