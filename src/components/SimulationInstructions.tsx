5ef153b48fa54c747b1c239379967cd5b1173e7d
remilebeau <remilebeau90@gmail.com>
2025-01-16 18:18:57 -0500

    update SimulationInstructions.tsx

--- src/components/SimulationInstructions.tsx
+++ src/components/SimulationInstructions.tsx
@@ -2,24 +2,23 @@ export default function SimulationInstructions() {
   return (
     <article className="flex flex-col rounded-xl border p-4">
       <p className="mb-2">
-        Model a production planning scenario with the inputs below. The model
-        will be simulated 1000 times, and the resulting 1000 profits will be
-        summarized in a histogram.
+        Model a production planning scenario with the form below.
       </p>
       <p className="mb-2">
-        The model and histogram provide insight about the scenario, such as:
+        The simulation provides insight about the scenario, such as:
       </p>
       <ul className="flex flex-col gap-x-2 sm:grid sm:grid-cols-2">
         <div className="flex flex-col">
           <li>What is the expected profit?</li>
           <li>What is the highest profit observed?</li>
-          <li>What is the chance of a profit higher than $X?</li>
-          <li>How sensitive is the profit to changes in the inputs?</li>
+          <li>What is the chance of a profit over $X?</li>
+          <li>Which inputs affect profit the most?</li>
         </div>
         <div className="flex-flex-col">
+          <li>Which inputs affect profit the least?</li>
           <li>What is the value at risk?</li>
           <li>What is the lowest profit observed?</li>
-          <li>What is the chance of a profit less than $X?</li>
+          <li>What is the chance of a profit under $X?</li>
         </div>
       </ul>
     </article>
