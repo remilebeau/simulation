705ae596e68c2e78a460460b57168c4a42cae1b2
remilebeau <remilebeau90@gmail.com>
2025-02-22 20:29:55 -0500

    fix header

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <main className="mx-auto flex max-w-4xl flex-col gap-4 p-8">
       <h1 className="text-center text-4xl font-bold">
-        Simulation for Production Planning
+        Production Planning Simulation
       </h1>
       <SimulationInstructions />
       <hr />
